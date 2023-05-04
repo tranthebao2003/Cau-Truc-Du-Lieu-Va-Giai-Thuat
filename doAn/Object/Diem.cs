22ef0b989f60b4af7473973217ee2a5ecb937fa6
sang le <sangdaole31102002@gmail.com>
2023-05-04 21:41:19 +0700

    4/5/2023 note code

--- doAn/Object/Diem.cs
+++ doAn/Object/Diem.cs
@@ -9,7 +9,7 @@ namespace doAn.Object
     internal class Diem
     {
         public string maMonHoc { get; set; } // 10 kí tự và 3 số cuối phải là số vd: abcdefg002
-        public int soLanThi { get; set; } // 1->
+        public int soLanThi { get; set; } // 1-> ( get, set ?)
         public float diem { get; set; } //0->10
 
         public Diem()
@@ -18,9 +18,9 @@ namespace doAn.Object
         }
         public Diem(string maMonHoc, int soLanThi, float diem)
         {
-            this.maMonHoc = maMonHoc;
-            this.soLanThi = soLanThi;
-            this.diem = diem;
+            this.maMonHoc = maMonHoc;//note
+            this.soLanThi = soLanThi;//note
+            this.diem = diem;//note
         }
     }
 }
